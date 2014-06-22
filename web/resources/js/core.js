9a28dfb9e98a8f14358d33a8e2d2c7f8319a52e9
Sergiu <sol.sergiu@gmail.com>
2014-06-22 11:29:35 +0300

    various fixes

--- web/resources/js/core.js
+++ web/resources/js/core.js
@@ -55,6 +55,7 @@ angular
                 $scope.$watch(target, function (value) {
                     var chart = new google.visualization.AnnotationChart($elm[0]);
                     if(value){
+                        $elm[0].html = "";
                         chart.draw(value.data, value.options);
                     }
                 }, true);
